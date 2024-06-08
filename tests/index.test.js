cfb8fd58f5174d5dfa9955bb6e7c8f7db941c71c
ayecue <soerenwehmeier@googlemail.com>
2024-06-08 13:01:54 +0200

    4.0.0 add test cases + lint variations

--- tests/index.test.js
+++ tests/index.test.js
@@ -4,4 +4,14 @@ describe('greyscriptMeta', () => {
   test('should return signatures', () => {
     expect(greyscriptMeta.getTypeSignature('general')).not.toBeNull();
   });
+
+  test('should return multiple matches', () => {
+    const matches = greyscriptMeta.searchDefinitionMatches(['map', 'string', 'list'], 'hasIndex');
+    expect([...matches.keys()]).toEqual(['map', 'string', 'list']);
+  });
+
+  test('should return parent match', () => {
+    const matches = greyscriptMeta.searchDefinitionMatches('file', 'hasIndex');
+    expect([...matches.keys()]).toEqual(['map']);
+  });
 });
